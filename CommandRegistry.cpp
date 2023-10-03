316b170f843588efe6d2c2e0243c4d6eb7dd7ea1
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-10-04 00:47:12 +0400

    CommandRegistry

---
+++ CommandRegistry.cpp
@@ -0,0 +1,15 @@
+#include "CommandRegistry.hpp"
+
+#include<string>
+
+CommandRegistry::CommandRegistry() :
+    ValidCommands{ "Add", "Remove", "Quit", "Display", "Save", "Load" } {}
+
+bool CommandRegistry::isValidCommand(const std::string& commandName) {
+    for (const auto& cmd : ValidCommands) {
+        if (commandName == cmd) {
+            return true;
+        }
+    }
+    return false;
+}
