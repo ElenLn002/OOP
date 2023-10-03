aaa7cd3b1f16d5ea06d4a5399be8b59404957ac7
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-10-04 01:02:50 +0400

    Tokenizer

---
+++ Tokenizer.cpp
@@ -0,0 +1,13 @@
+#include "Tokenizer.hpp"
+
+void Tokenizer::takeTokens(const std::string& input) {
+    std::istringstream iss(input);
+    std::string token;
+    while (iss >> token) {
+        tokens.push_back(token);
+    }
+}
+
+std::vector<std::string> Tokenizer::getTokens() {
+    return tokens;
+}
