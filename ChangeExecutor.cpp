b1b85095a8e86e562db4b81af0da5d440e317257
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-10-07 18:52:49 +0400

    ChangeExecutor.cpp

--- ChangeExecutor.cpp
+++ ChangeExecutor.cpp
@@ -7,7 +7,7 @@ void ChangeExecutor::execute(std::map<int, std::string>& itemsMap) {
     auto it = itemsMap.find(itemIndex);
     if (it != itemsMap.end()) {
         it->second = newArguments;
-        std::cout << "Item with index " << itemIndex << " changed." << std::endl;
+        
     }
     else {
         std::cerr << "Item with index " << itemIndex << " not found. No item changed." << std::endl;
