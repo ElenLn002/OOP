b3f2d60af0a1ddeb3f929b2f5ef0365fbcd9c384
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-11-03 22:24:14 +0400

    Parser.cpp

--- Parser.cpp
+++ Parser.cpp
@@ -1,22 +1,8 @@
 #include "Parser.hpp"
 
 Parser::Parser(const std::string& input, CommandRegistry& registry) :
-    input(input), registry(registry), validCommand(false) {}
-
-std::string Parser::getCommand() const {
-    return command;
-}
-
-std::vector<std::string> Parser::getArguments() const {
-    return arguments;
-}
-
-std::string Parser::getRestOfTheLine() const {
-    return restOfTheLine;
-}
-
-bool Parser::isValidCommand() const {
-    return validCommand;
+    input(input), registry(registry), validCommand(false) {
+    parse();
 }
 
 void Parser::parse() {
@@ -29,22 +15,18 @@ void Parser::parse() {
         if (registry.isValidCommand(possibleCommand)) {
             validCommand = true;
             command = possibleCommand;
-
-            // Store the arguments and the rest of the line
-            arguments.clear();
-            restOfTheLine.clear();
-            for (size_t i = 1; i < tokens.size(); ++i) {
-                if (i == 1) {
-                    restOfTheLine += tokens[i];
-                }
-                else {
-                    restOfTheLine += " " + tokens[i];
-                }
-                arguments.push_back(tokens[i]);
-            }
         }
-        else {
-            std::cerr << "Invalid command." << std::endl;
+
+        items.clear();
+        restOfLine.clear();
+        for (size_t i = 1; i < tokens.size(); ++i) {
+            if (i == 1) {
+                restOfLine += tokens[i];
+            }
+            else {
+                restOfLine += " " + tokens[i];
+            }
+            items.push_back(tokens[i]);
         }
     }
 }
