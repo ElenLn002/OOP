6baa6656970920e0243aab25b5b75534bf11a5b0
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-11-03 22:02:22 +0400

    ListExecutor.cpp

---
+++ ListExecutor.cpp
@@ -0,0 +1,8 @@
+#include "ListExecutor.hpp"
+
+ListExecutor::ListExecutor(const std::map<int, std::string>& itemsMap)
+    : itemsMap(itemsMap) {}
+
+void ListExecutor::execute(std::map<int, std::string>& itemsMap) {
+    resultPrinter.printItems(itemsMap); // Delegate the printing to ResultPrinter
+}
