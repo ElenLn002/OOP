da36b173f797440211e7c7daeb2f02628329ae04
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-11-03 23:45:02 +0400

    SaveExecutor.cpp

--- SaveExecutor.cpp
+++ SaveExecutor.cpp
@@ -1,22 +1,19 @@
 #include "RemoveExecutor.hpp"
 
-SaveExecutor::SaveExecutor(const std::string& filePath, const std::map<int, std::string>& itemsMap)
-    : filePath(filePath), itemsMap(itemsMap) {}
+SaveExecutor::SaveExecutor(std::string filePath, std::map<int, std::string>& itemsMap) :
+    filePath(filePath), itemsMap(itemsMap) {}
 
-void SaveExecutor::execute() {
+void SaveExecutor::execute(std::map<int, std::string>& itemsMap) {
     std::ofstream outputFile(filePath);
-
     if (!outputFile.is_open()) {
-        std::cerr << "Unable to open file for saving." << std::endl;
+        std::cerr << "Unable to open file" << std::endl;
         return;
     }
 
-    // Write the content of the itemsMap to the file
     for (const auto& entry : itemsMap) {
         outputFile << entry.first << " " << entry.second << std::endl;
     }
 
     std::cout << "Content saved to file: " << filePath << std::endl;
-
     outputFile.close();
 }
