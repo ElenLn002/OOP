3633f8a8c7ed45cb1e032a42303a800130acd809
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-11-03 22:27:43 +0400

    ShapeRegistry.cpp

--- ShapeRegistry.cpp
+++ ShapeRegistry.cpp
@@ -1,11 +1,11 @@
 #include "ShapeRegistry.hpp"
 
 ShapeRegistry::ShapeRegistry() :
-    ValidShapes{ "rectangle", "line", "circle", "trapezoid", "elipse" } {}
+    validShapes{ "rectangle", "ellipse", "line", "triangle", "trapezoid" } {}
 
-bool ShapeRegistry::isValidShape(const std::string& shapeName) {
-    for (const auto& cmd : ValidShapes) {
-        if (shapeName == cmd) {
+bool ShapeRegistry::isValidShape(const std::string& inputShape) {
+    for (const auto& input : validShapes) {
+        if (inputShape == input) {
             return true;
         }
     }
