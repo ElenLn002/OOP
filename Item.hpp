7ab222243f193abe4847fea6b2d15eb779b181a1
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-11-03 22:35:25 +0400

    Item.hpp

---
+++ Item.hpp
@@ -0,0 +1,27 @@
+#ifndef ITEM_HPP
+#define ITEM_HPP
+
+#include <string>
+
+using shapeType = std::string;
+
+class Item {
+public:
+    Item(int ID, const shapeType& shape, const std::string& pos, const std::string& attributes);
+
+    shapeType getShape() const;
+    std::string getPosition() const;
+    std::string getAttributes() const;
+    int getID() const;
+    
+    void setAttributes(const std::string& attributes);
+    void setPosition(const std::string& pos);
+
+private:
+    std::string attributes;
+    shapeType shape;
+    std::string pos;
+    int ID;
+};
+
+#endif //ITEM_HPP
