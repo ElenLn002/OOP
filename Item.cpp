d5964fcd825c37647280bfc5f3ca8fec1e1bca86
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-11-03 22:39:46 +0400

    Item.cpp

---
+++ Item.cpp
@@ -0,0 +1,28 @@
+#include "Item.hpp"
+
+int Item::getID() const {
+    return ID;
+}
+
+Item::Item(int ID, const shapeType& shape, const std::string& pos, const std::string& attributes) :
+    ID(ID), shape(shape), pos(pos), attributes(attributes) {}
+
+shapeType Item::getShape() const {
+    return shape;
+}
+
+std::string Item::getPosition() const {
+    return pos;
+}
+
+std::string Item::getAttributes() const {
+    return attributes;
+}
+
+void Item::setAttributes(const std::string& attributes) {
+    this->attributes = attributes;
+}
+
+void Item::setPosition(const std::string& pos) {
+    this->pos = pos;
+}
