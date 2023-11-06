0b3849dda71a92bf4e67db25c088ff358a0aad41
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-11-06 22:37:41 +0400

    RemoveExecutor.cpp

--- CommandLineInterface/RemoveExecutor.cpp
+++ CommandLineInterface/RemoveExecutor.cpp
@@ -1,13 +1,24 @@
 #include "RemoveExecutor.hpp"
 
-RemoveExecutor::RemoveExecutor(int ID) : itemID(ID) {}
-
 void RemoveExecutor::execute(std::map<int, std::string>& itemsMap) {
     auto it = itemsMap.find(itemID);
     if (it != itemsMap.end()) {
         itemsMap.erase(it);
+
+        // Create a new map with adjusted IDs
+        std::map<int, std::string> newItemsMap;
+        for (const auto& item : itemsMap) {
+            if (item.first > itemID) {
+                newItemsMap[item.first - 1] = item.second;
+            }
+            else {
+                newItemsMap[item.first] = item.second;
+            }
+        }
+
+        itemsMap = newItemsMap;
     }
     else {
-        std::cerr << "ID not found" << std::endl;
+        std::cerr << "ID not found: " << itemID << std::endl;
     }
 }
