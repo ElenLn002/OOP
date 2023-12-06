85332dd09d0cb36359b03a18bd2952b4fe8fe94a
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-06 23:02:29 +0400

    SaveExecutor.hpp

--- CLI/Commands/SaveExecutor.hpp
+++ CLI/Commands/SaveExecutor.hpp
@@ -1,19 +1,11 @@
 #ifndef SAVEEXECUTOR_HPP
 #define SAMOVEEXECUTOR_HPP
 
-#include <string>
-#include <map>
+#include "CommandExecutor.hpp"
 
-#include "SerializingInterface.hpp"
-
-class SaveExecutor : public SerializingInterface {
+class SaveExecutor : public CommandExecutor {
 public:
-    SaveExecutor(std::string filePath, std::map<int, std::string>& itemsMap);
-    void execute(std::map<int, std::string>& itemsMap) override;
-
-private:
-    std::string filePath;
-    std::map<int, std::string>& itemsMap;
+    void execute() override;
 };
 
 #endif //SAMOVEEXECUTOR_HPP
