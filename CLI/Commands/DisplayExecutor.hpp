07edf795c33eeb8807ccd9375e0f9b065f355d91
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-06 22:56:03 +0400

    DisplayExecutor.hpp

--- CLI/Commands/DisplayExecutor.hpp
+++ CLI/Commands/DisplayExecutor.hpp
@@ -1,20 +1,9 @@
 #ifndef DISPLAYEXECUTOR_HPP
 #define DISPLAYEXECUTOR_HPP
 
-#include <map>
-#include <string>
-
-#include "RenderingInterface.hpp"
-
-class DisplayExecutor : public RenderingInterface {
+class DisplayExecutor : public CommandExecutor {
 public:
-    DisplayExecutor(int ID, const std::map<int, std::string>& itemsMap);
-    void execute(std::map<int, std::string>& itemsMap) override;
-
-private:
-    int itemID;
-    const std::map<int, std::string>& itemsMap;
+    void execute() override;
 };
-
 
 #endif //DISPLAYEXECUTOR_HPP
