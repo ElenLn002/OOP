fd1b0e51da7b4a60e7c3ddb831f5e599fa7ead77
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-06 22:51:10 +0400

    CommandExecutor.hpp

--- CLI/Commands/CommandExecutor.hpp
+++ CLI/Commands/CommandExecutor.hpp
@@ -6,7 +6,23 @@
 
 class CommandExecutor {
 public:
-    virtual void execute(std::map<int, std::string>& itemsMap) = 0;
+    virtual void execute() = 0;
+
+protected:
+
+    bool isItem(const std::string& type);
+    bool isSlide(const std::string& type);
+
+//this list is temporary
+    std::map<int, std::string>& itemsMap;
+    std::string addedAttributes;
+    int ID;
+    std::string newAttributes;
+    int* initialItemID;
+    std::string filePath;
+    std::map<int, std::string>& itemsMap;
+    int itemID;
+    const std::string& type;
 };
 
 #endif // COMMANDEXECUTOR_HPP
