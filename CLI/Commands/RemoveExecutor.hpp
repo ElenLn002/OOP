a7b90d4122fca1f4a5e460ab8dee058e5610b35b
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-06 22:54:43 +0400

    RemoveExecutor.hpp

--- CLI/Commands/RemoveExecutor.hpp
+++ CLI/Commands/RemoveExecutor.hpp
@@ -1,16 +1,9 @@
 #ifndef REMOVEEXECUTOR_HPP
 #define REMOVEEXECUTOR_HPP
 
-#include <string>
-#include <map>
-
 class RemoveExecutor : public CommandExecutor {
 public:
-    RemoveExecutor(int ID);
-    void execute(std::map<int, std::string>& itemsMap) override;
-
-private:
-    int itemID;
+    void execute() override;
 };
 
 #endif //REMOVEEXECUTOR_HPP
