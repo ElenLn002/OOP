fa3f805c15bb0295f30d35236735044b140702d6
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-06 23:45:29 +0400

    RemoveItemFromSlide.cpp

---
+++ Director/RemoveItemFromSlide.cpp
@@ -0,0 +1,17 @@
+#include "RemoveITemFromSlide.hpp"
+#include "Slide.hpp"
+
+std::shared_ptr<IAction> RemoveItemFromSlide:: execute(std::shared_ptr<Document>& document)  {
+    std::shared_ptr<Slide> slide = document->getSlide(slideNumber);
+
+    if (slide) {
+        slide->removeItem(itemID); // Assuming removeItem method in Slide takes itemID
+        std::cout << "Item with ID " << itemID << " removed from slide " << slideNumber << std::endl;
+    }
+    else {
+        std::cout << "Slide " << slideNumber << " does not exist." << std::endl;
+    }
+
+    // Return null for now
+    return nullptr; // It should return AddItemToSlide
+}
