5b9afb9ee394160db720382c31192beefae91b70
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-06 22:55:03 +0400

    RemoveExecutor.cpp

--- CLI/Commands/RemoveExecutor.cpp
+++ CLI/Commands/RemoveExecutor.cpp
@@ -1,24 +1,10 @@
 #include "RemoveExecutor.hpp"
 
-void RemoveExecutor::execute(std::map<int, std::string>& itemsMap) {
-    auto it = itemsMap.find(itemID);
-    if (it != itemsMap.end()) {
-        itemsMap.erase(it);
-
-        // Create a new map with adjusted IDs
-        std::map<int, std::string> newItemsMap;
-        for (const auto& item : itemsMap) {
-            if (item.first > itemID) {
-                newItemsMap[item.first - 1] = item.second;
-            }
-            else {
-                newItemsMap[item.first] = item.second;
-            }
-        }
-
-        itemsMap = newItemsMap;
+void RemoveExecutor::execute() {
+    if (isItem(type)) {
+        ///TODO: item remove
     }
-    else {
-        std::cerr << "ID not found: " << itemID << std::endl;
+    else if (isSlide(type)) {
+        ///TODO::slide remove
     }
 }
