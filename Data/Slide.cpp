f77c0e6338266c85281ae3391da9f3812441637c
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-06 23:10:29 +0400

    Slide.cpp

--- Data/Slide.cpp
+++ Data/Slide.cpp
@@ -4,6 +4,28 @@ void Slide::addItem(const std::shared_ptr<Item>& item) {
     items.push_back(item);
 }
 
-const std::vector<std::shared_ptr<Item>>& Slide::getItems() const {
-    return items;
+const std::vector<std::shared_ptr<Item>> Slide::getItem(int slideID) const {
+    std::vector<std::shared_ptr<Item>> foundItems;
+
+    for (const auto& item : items) {
+        if (item->getID() == slideID) {
+            foundItems.push_back(item);
+        }
+    }
+
+    return foundItems;
+}
+
+void Slide::removeItem(int itemID) {
+    auto it = std::find_if(items.begin(), items.end(), [itemID](const std::shared_ptr<Item>& item) {
+        return item->getID() == itemID;
+        });
+
+    if (it != items.end()) {
+        items.erase(it);
+        std::cout << "Item with ID " << itemID << " removed from slide." << std::endl;
+    }
+    else {
+        std::cout << "Item with ID " << itemID << " not found in the slide." << std::endl;
+    }
 }
