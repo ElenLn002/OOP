870b381843d5652d2c706d77592cb11d883ca335
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-06 22:55:35 +0400

    ChangeExecutor.hpp

--- CLI/Commands/ChangeExecutor.hpp
+++ CLI/Commands/ChangeExecutor.hpp
@@ -1,17 +1,9 @@
 #ifndef CHANGEEXECUTOR_HPP
 #define CHANGEEXECUTOR_HPP
 
-#include <map>
-#include<string>
-
 class ChangeExecutor : public CommandExecutor {
 public:
-    ChangeExecutor(int ID, std::string& newAttributes);
-    void execute(std::map<int, std::string>& itemsMap) override;
-
-private:
-    int itemID;
-    std::string newAttributes;
+    void execute() override;
 };
 
 #endif // CHANGEEXECUTOR_HPP
