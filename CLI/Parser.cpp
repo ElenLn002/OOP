efb85199edda15629e336a84e9d71c6e481a1edc
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-08 23:34:58 +0400

    Parser.cpp

--- CLI/Parser.cpp
+++ CLI/Parser.cpp
@@ -1,32 +1,67 @@
 #include "Parser.hpp"
 
-Parser::Parser(const std::string& input, CommandRegistry& registry) :
-    input(input), registry(registry), validCommand(false) {
-    parse();
+Parser::Parser(const std::string& input, CommandRegistry& registry)
+    : input(input), registry(registry), validCommand(false) {
+    parseInput();
 }
 
-void Parser::parse() {
+void Parser::parseInput() {
+    std::istringstream iss(input);
     Tokenizer tokenizer;
-    tokenizer.takeTokens(input);
+    tokenizer.takeTokens(iss);  // Pass the stream to takeTokens
     std::vector<std::string> tokens = tokenizer.getTokens();
 
     if (!tokens.empty()) {
-        std::string possibleCommand = tokens[0];
-        if (registry.isValidCommand(possibleCommand)) {
-            validCommand = true;
-            command = possibleCommand;
-        }
+        command = tokens[0];
+        validCommand = registry.isValidCommand(command);
 
-        items.clear();
-        restOfLine.clear();
+        // Clear the attributes map before populating it
+        attributesMap.clear();
+
+        // Assuming attributes start from the second token onwards
         for (size_t i = 1; i < tokens.size(); ++i) {
-            if (i == 1) {
-                restOfLine += tokens[i];
-            }
-            else {
-                restOfLine += " " + tokens[i];
-            }
-            items.push_back(tokens[i]);
+            // Store each attribute in the unordered map with its corresponding index
+            attributesMap[i - 1] = tokens[i];
         }
     }
+}
+
+std::unique_ptr<Command> Parser::parse() {
+    if (!validCommand) {
+        std::cout << "Invalid command." << std::endl;
+        return nullptr;
+    }
+
+    // Create the appropriate command based on the parsed input
+    std::unique_ptr<Command> commandObj;
+    if (command == "Add") {
+        commandObj = std::make_unique<Add>();
+    }
+    else if (command == "Remove") {
+        commandObj = std::make_unique<Remove>();
+    }
+    else if (command == "Change") {
+        commandObj = std::make_unique<Change>();
+    }
+    else if (command == "Display") {
+        commandObj = std::make_unique<Display>();
+    }
+    else if (command == "List") {
+        commandObj = std::make_unique<List>();
+    }
+    else if (command == "Save") {
+        commandObj = std::make_unique<Save>();
+    }
+    else if (command == "Load") {
+        commandObj = std::make_unique<Load>();
+    }
+    else if (command == "Quit") {
+        commandObj = std::make_unique<Quit>();
+    }
+    else {
+        std::cout << "Unknown command." << std::endl;
+        return nullptr;
+    }
+
+    return commandObj;
 }
