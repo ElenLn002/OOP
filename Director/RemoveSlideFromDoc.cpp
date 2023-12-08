b36803144cd06a03d3db14f480413c24c8c7a710
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-08 23:45:29 +0400

    RemoveSlideFromDoc.cpp

---
+++ Director/RemoveSlideFromDoc.cpp
@@ -0,0 +1,10 @@
+#include "RemoveSlideFromDoc.cpp"
+#include "Includes.hpp"
+
+std::shared_ptr<IAction> RemoveSlideFromDoc:: execute()  {
+    std::shared_ptr<Document> document = Application::getDocument();
+    document->removeSlide(slideNumber);
+    std::cout << "Slide with ID " << slideNumber << " removed from document." << std::endl;
+
+    return nullptr;
+}
