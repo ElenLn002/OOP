a6c7476ce829c34845c09201fd4362a8adbbf532
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-09 14:52:59 +0400

    SaveDocument.cpp

---
+++ Serializing/SaveDocument.cpp
@@ -0,0 +1,25 @@
+#include "SaveDocument.hpp"
+
+#include "Includes.hpp"
+
+void SaveDocument::execute(const std::string& filename, std::shared_ptr<Document> doc) {
+    std::ofstream outFile(filename); 
+    if (outFile.is_open()) {
+        outFile << "Document Content:" << std::endl;
+        for (size_t i = 1; i <= doc->getNumSlides(); ++i) {
+            auto slide = doc->getSlide(i);
+            outFile << "Slide " << i << ":\n";
+            if (slide) {
+                for (const auto& item : slide->itemsMap) {
+                    outFile << "Item ID: " << item.first << ", Shape: " << item.second->getShape()
+                        << ", Attributes: " << item.second->getAttributes() << "\n";
+                }
+            }
+        }
+        outFile.close();
+        std::cout << "Document saved to " << filename << std::endl;
+    }
+    else {
+        std::cerr << "Unable to open file: " << filename << std::endl;
+    }
+}
