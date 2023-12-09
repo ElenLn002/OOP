e945f446c410dcd8418f3d497e8e9831e2b70ab4
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-09 14:51:25 +0400

    Update and rename SaveExecutor.hpp to SaveDocument.hpp

---
+++ Serializing/SaveDocument.hpp
@@ -0,0 +1,15 @@
+#ifndef SAVEDOCUMENT_HPP
+#define SAVEDOCUMENT_HPP
+
+#include <string>
+#include <memory>
+
+#include "ISerialize.hpp"
+#include "Document.hpp"
+
+class SaveDocument {
+public:
+    void execute(const std::string& filename, std::shared_ptr<Document> doc);
+};
+
+#endif //SAVEDOCUMENT_HPP
