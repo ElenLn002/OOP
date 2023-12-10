f536e90869c37db1e35198b2c7aa43bc487b7013
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-10 19:07:33 +0400

    LoadExecutor.cpp

--- Serializing/LoadExecutor.cpp
+++ Serializing/LoadExecutor.cpp
@@ -1,32 +1,35 @@
 #include "LoadDocument.hpp"
 #include "Includes.hpp"
 
-//will be changed
-void LoadExecutor::execute(std::map<int, std::string>& itemsMap) {
-    std::ifstream inputFile(filePath);
 
-    if (!inputFile.is_open()) {
-        std::cerr << "Unable to open the specified file for loading." << std::endl;
-        return;
-    }
-
-    std::string line;
-    int nextID = 0;
-
-    while (std::getline(inputFile, line)) {
-        std::istringstream lineStream(line);
-        int itemID;
-        lineStream >> itemID;
-
-        if (lineStream) {
-            *initialItemID = std::max(*initialItemID, itemID + 1); 
-            std::string attributes;
-            std::getline(lineStream, attributes);
-            itemsMap[itemID] = attributes;
+void LoadDocument::execute(std::string& filename, std::shared_ptr<Document> doc) {
+    std::ifstream inFile(filename); // Open the file for reading
+    if (inFile.is_open()) {
+        std::string line;
+        std::shared_ptr<Slide> currentSlide;
+        while (std::getline(inFile, line)) {
+            if (line.find("Slide") != std::string::npos) {
+                // Create a new slide and add it to the document
+                currentSlide = std::make_shared<Slide>();
+                doc->addSlide(currentSlide);
+            }
+            else if (line.find("Item ID") != std::string::npos && currentSlide) {
+                // Parse item properties from the line
+                std::istringstream iss(line);
+                int id;
+                std::string shape, attributes;
+                Coordinate pos; // You'll need to define how to parse this
+                iss >> id >> shape >> pos >> attributes; // Adjust this line based on your file format
+                // Create a new item and add it to the current slide
+                auto item = std::make_shared<Item>(id, shape, pos, attributes);
+                currentSlide->addItem(item);
+            }
+            // Parse other lines as needed
         }
+        inFile.close();
+        std::cout << "Document loaded from " << filename << std::endl;
     }
-
-    inputFile.close();
-
-    std::cout << "Commands loaded from file: " << filePath << std::endl;
+    else {
+        std::cerr << "Unable to open file: " << filename << std::endl;
+    }
 }
