bb2df0f37e90e6d23bff2e65b1451d9c60488152
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-20 16:44:29 +0400

    Change.hpp

---
+++ CLI/Commands/Change.hpp
@@ -0,0 +1,9 @@
+#ifndef CHANGE_HPP
+#define CHANGE_HPP
+
+class Change : public Command {
+public:
+    void execute() override;
+};
+
+#endif // CHANGEEXECUTOR_HPP
--- CLI/Commands/ChangeExecutor.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CHANGEEXECUTOR_HPP
-#define CHANGEEXECUTOR_HPP
-
-class ChangeExecutor : public Command {
-public:
-    void execute() override;
-};
-
-#endif // CHANGEEXECUTOR_HPP
