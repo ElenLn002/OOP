cca3fe8ce9b11e6bdeb3aa5b737864421cb150bb
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2023-12-20 16:19:53 +0400

    Slide.cpp

--- Data/Slide.cpp
+++ Data/Slide.cpp
@@ -1,31 +1,24 @@
 #include "Slide.hpp
 
 void Slide::addItem(const std::shared_ptr<Item>& item) {
-    items.push_back(item);
-}
-
-const std::vector<std::shared_ptr<Item>> Slide::getItem(int slideID) const {
-    std::vector<std::shared_ptr<Item>> foundItems;
-
-    for (const auto& item : items) {
-        if (item->getID() == slideID) {
-            foundItems.push_back(item);
-        }
-    }
-
-    return foundItems;
+    itemsMap[item->getID()] = item;
 }
 
 void Slide::removeItem(int itemID) {
-    auto it = std::find_if(items.begin(), items.end(), [itemID](const std::shared_ptr<Item>& item) {
-        return item->getID() == itemID;
-        });
-
-    if (it != items.end()) {
-        items.erase(it);
+    auto it = itemsMap.find(itemID);
+    if (it != itemsMap.end()) {
+        itemsMap.erase(it);
         std::cout << "Item with ID " << itemID << " removed from slide." << std::endl;
     }
     else {
         std::cout << "Item with ID " << itemID << " not found in the slide." << std::endl;
     }
+}
+
+std::shared_ptr<Item> Slide::getItem(int itemID) const {
+    auto it = itemsMap.find(itemID);
+    if (it != itemsMap.end()) {
+        return it->second;
+    }
+    return nullptr;
 }
