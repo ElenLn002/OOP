99301728505db946c5289b6e1c6dcd0d6041d954
ElenLn002 <112886676+ElenLn002@users.noreply.github.com>
2024-01-07 18:22:49 +0400

    Parser.cpp

--- CLI/Parser/Parser.cpp
+++ CLI/Parser/Parser.cpp
@@ -5,32 +5,6 @@ Parser::Parser(const std::string& input, CommandFactory& factory)
     parseInput();
 }
 
-void Parser::parseInput() {
-    std::istringstream iss(input);
-    Tokenizer tokenizer;
-    tokenizer.takeTokens(iss);
-    std::vector<std::string> tokens = tokenizer.getTokens();
-
-    if (!tokens.empty()) {
-        command = tokens[0];
-        // Factory does not have isValidCommand, assuming we need to create the command to check validity
-        std::unique_ptr<Command> tempCommand = factory.createCommand(command);
-
-        if (tempCommand) {
-            validCommand = true;
-
-            attributesMap.clear();
-            // Extract attributes if available
-            for (size_t i = 1; i < tokens.size(); ++i) {
-                attributesMap[i - 1] = tokens[i];
-            }
-        }
-        else {
-            validCommand = false;
-        }
-    }
-}
-
 std::unique_ptr<Command> Parser::parse() {
     if (!validCommand) {
         std::cout << "Invalid command." << std::endl;
